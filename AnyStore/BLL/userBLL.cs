f561a0e25313ceef84b94d031d41d2535fd54813
Vijay Thapa <vijaythapa333@gmail.com>
2017-11-13 09:49:57 +0545

    User BLL
    User BLL Successfully Created for our Billing and Inventory Management System.

---
+++ AnyStore/BLL/userBLL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyStore.BLL
+{
+    class userBLL
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string contact { get; set; }
+        public string address { get; set; }
+        public string gender { get; set; }
+        public string user_type { get; set; }
+        public DateTime added_date { get; set; }
+        public int added_by { get; set; }
+
+    }
+}
