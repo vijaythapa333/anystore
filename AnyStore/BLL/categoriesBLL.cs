d19f6a082e6c98fd0b823c195884484787e1ee21
Vijay Thapa <vijaythapa333@gmail.com>
2018-01-11 11:17:50 +0545

    BLL for Category Module
    Business Logic Layer (BLL) for our Category Module created Successfully.
    It contains Getter and Setter Properties for our Category Module.

---
+++ AnyStore/BLL/categoriesBLL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyStore.BLL
+{
+    class categoriesBLL
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public DateTime added_date { get; set; }
+        public int added_by { get; set; }
+
+    }
+}
