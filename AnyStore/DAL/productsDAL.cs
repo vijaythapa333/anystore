2046f8ab4b66243930678058cdd6b1eb18af367f
Vijay Thapa <vijaythapa333@gmail.com>
2018-01-23 13:55:52 +0545

    DAL for Product Module
    Created Methods to Insert new Product, Update & Delete Existing Product for Product Module in our Billing and Inventory Management System.

---
+++ AnyStore/DAL/productsDAL.cs
@@ -0,0 +1,216 @@
+﻿using AnyStore.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore.DAL
+{
+    class productsDAL
+    {
+        //Creating STATI String Method for DB Connection
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        #region Select method for Product Module
+        public DataTable Select()
+        {
+            //Create Sql Connection to connect Databaes
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            //DAtaTable to hold the data from database
+            DataTable dt = new DataTable();
+
+            try
+            {
+                //Writing the Query to Select all the products from database
+                String sql = "SELECT * FROM tbl_products";
+
+                //Creating SQL Command to Execute Query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //SQL Data Adapter to hold the value from database temporarily
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                //Open DAtabase Connection
+                conn.Open();
+
+                adapter.Fill(dt);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return dt;
+        }
+        #endregion
+        #region Method to Insert Product in database
+        public bool Insert(productsBLL p)
+        {
+            //Creating Boolean Variable and set its default value to false
+            bool isSuccess = false;
+
+            //Sql Connection for DAtabase
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //SQL Query to insert product into database
+                String sql = "INSERT INTO tbl_products (name, category, description, rate, qty, added_date, added_by) VALUES (@name, @category, @description, @rate, @qty, @added_date, @added_by)";
+
+                //Creating SQL Command to pass the values
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Passign the values through parameters
+                cmd.Parameters.AddWithValue("@name", p.name);
+                cmd.Parameters.AddWithValue("@category", p.category);
+                cmd.Parameters.AddWithValue("@description", p.description);
+                cmd.Parameters.AddWithValue("@rate", p.rate);
+                cmd.Parameters.AddWithValue("@qty", p.qty);
+                cmd.Parameters.AddWithValue("@added_date", p.added_date);
+                cmd.Parameters.AddWithValue("@added_by", p.added_by);
+
+                //Opening the Database connection
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                //If the query is executed successfully then the value of rows will be greater than 0 else it will be less than 0
+                if(rows>0)
+                {
+                    //Query Executed Successfully
+                    isSuccess = true;
+                }
+                else
+                {
+                    //FAiled to Execute Query
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+        #region Method to Update Product in Database
+        public bool Update(productsBLL p)
+        {
+            //create a boolean variable and set its initial value to false
+            bool isSuccess = false;
+
+            //Create SQL Connection for DAtabase
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //SQL Query to Update Data in dAtabase
+                String sql = "UPDATE tbl_products SET name=@name, category=@category, description=@description, rate=@rate, qty=@qty, added_date=@added_date, added_by=@added_by WHERE id=@id";
+
+                //Create SQL Cmmand to pass the value to query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //Passing the values using parameters and cmd
+                cmd.Parameters.AddWithValue("@name", p.name);
+                cmd.Parameters.AddWithValue("@category", p.category);
+                cmd.Parameters.AddWithValue("@description", p.description);
+                cmd.Parameters.AddWithValue("@rate", p.rate);
+                cmd.Parameters.AddWithValue("@qty", p.qty);
+                cmd.Parameters.AddWithValue("@added_date", p.added_date);
+                cmd.Parameters.AddWithValue("@added_by", p.added_by);
+                cmd.Parameters.AddWithValue("@id", p.id);
+
+                //Open the Database connection
+                conn.Open();
+
+                //Create Int Variable to check if the query is executed successfully or not
+                int rows = cmd.ExecuteNonQuery();
+
+                //if the query is executed successfully then the value of rows will be greater than 0 else it will be less than zero
+                if(rows>0)
+                {
+                    //Query ExecutedSuccessfully
+                    isSuccess = true;
+                }
+                else
+                {
+                    //Failed to Execute Query
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+        #region Method to Delete Product from Database
+        public bool Delete(productsBLL p)
+        {
+            //Create Boolean Variable and Set its default value to false
+            bool isSuccess = false;
+
+            //SQL Connection for DB connection
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //Write Query Product from DAtabase
+                String sql = "DELETE FROM tbl_products WHERE id=@id";
+
+                //Sql Command to Pass the Value
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Passing the values using cmd
+                cmd.Parameters.AddWithValue("@id", p.id);
+
+                //Open Database Connection
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+                //If the query is executed successfullly then the value of rows will be greated than 0 else it will be less than 0
+                if(rows>0)
+                {
+                    //Query Executed Successfully
+                    isSuccess = true;
+                }
+                else
+                {
+                    //Failed to Execute Query
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+    }
+}
