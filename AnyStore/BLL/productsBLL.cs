ce1c41efeb12f5ebf623b927ed10b5827fa425d9
Vijay Thapa <vijaythapa333@gmail.com>
2018-01-23 12:11:12 +0545

    BLL for Product Module
    Successfully Created Business Logic Layer Containing Getters and Setters for our BILLING AND INVENTORY MANAGEMENT SYSTEM IN C# AND MSSQL SERVER

---
+++ AnyStore/BLL/productsBLL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyStore.BLL
+{
+    class productsBLL
+    {
+        //Getters and Setters for Product Module
+        public int id { get; set; }
+        public string name { get; set; }
+        public string category { get; set; }
+        public string description { get; set; }
+        public decimal rate { get; set; }
+        public decimal qty { get; set; }
+        public DateTime added_date { get; set; }
+        public int added_by { get; set; }
+
+    }
+}
