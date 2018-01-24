6beadd497b276b815fa04f989c2711cd1a9a89f5
Vijay Thapa <vijaythapa333@gmail.com>
2018-01-24 11:30:23 +0545

    BLL for Dealer and Customer
    Getters and Setters for Dealer and Customer is created in BLL for our Billing and Inventory Management System

---
+++ AnyStore/BLL/DeaCustBLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyStore.BLL
+{
+    class DeaCustBLL
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string contact { get; set; }
+        public string address { get; set; }
+        public DateTime added_date { get; set; }
+        public int added_by { get; set; }
+
+    }
+}
