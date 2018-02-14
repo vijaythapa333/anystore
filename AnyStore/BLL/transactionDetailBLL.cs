d80e71fd82a8121f807ff2794fd78e8e8d4413a0
Vijay Thapa <vijaythapa333@gmail.com>
2018-02-14 13:09:15 +0545

    BLL for Transaction Module
    Here we created Getters and Setters for Transaction and Transaction Detail BLL (Business Logic Layer)

---
+++ AnyStore/BLL/transactionDetailBLL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyStore.BLL
+{
+    class transactionDetailBLL
+    {
+        public int id { get; set; }
+        public int product_id { get; set; }
+        public decimal rate { get; set; }
+        public decimal qty { get; set; }
+        public decimal total { get; set; }
+        public int dea_cust_id { get; set; }
+        public DateTime added_date { get; set; }
+        public int added_by { get; set; }
+    }
+}
---
+++ AnyStore/BLL/transactionsBLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyStore.BLL
+{
+    class transactionsBLL
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public int dea_cust_id { get; set; }
+        public decimal grandTotal { get; set; }
+        public DateTime transaction_date { get; set; }
+        public decimal tax { get; set; }
+        public decimal discount { get; set; }
+        public int added_by { get; set; }
+
+    }
+}
