f825e536bc506026e0fbb1a222d4cf4233f8e6a7
Vijay Thapa <vijaythapa333@gmail.com>
2018-02-15 11:27:02 +0545

    DAL for Transaction Module
    Here, we have created Data Access Layer (DAL) for Transaction Module where we create methods to insert transaction and transaction details.

---
+++ AnyStore/DAL/transactionDAL.cs
@@ -0,0 +1,77 @@
+﻿using AnyStore.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore.DAL
+{
+    class transactionDAL
+    {
+        //Create a connection string variable
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        #region Insert Transaction Method
+        public bool Insert_Transaction(transactionsBLL t, out int transactionID)
+        {
+            //Create a boolean value and set its default value to false
+            bool isSuccess = false;
+            //Set the out transactionID value to negative 1 i.e. -1
+            transactionID = -1;
+            //Create a SqlConnection first
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                //SQL Query to Insert Transactions
+                string sql = "INSERT INTO tbl_transactions (type, dea_cust_id, grandTotal, transaction_date, tax, discount, added_by) VALUES (@type, @dea_cust_id, @grandTotal, @transaction_date, @tax, @discount, @added_by)";
+
+                //Sql Commandto pass the value in sql query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Passing the value to sql query using cmd
+                cmd.Parameters.AddWithValue("@type", t.type);
+                cmd.Parameters.AddWithValue("@dea_cust_id", t.dea_cust_id);
+                cmd.Parameters.AddWithValue("@grandTotal", t.grandTotal);
+                cmd.Parameters.AddWithValue("@transaction_date", t.transaction_date);
+                cmd.Parameters.AddWithValue("@tax", t.tax);
+                cmd.Parameters.AddWithValue("@discount", t.discount);
+                cmd.Parameters.AddWithValue("@added_by", t.added_by);
+
+                //Open Database Connection
+                conn.Open();
+
+                //Execute the Query
+                object o = cmd.ExecuteScalar();
+
+                //If the query is executed successfully then the value will not be null else it will be null
+                if(o!=null)
+                {
+                    //Query Executed Successfully
+                    transactionID = int.Parse(o.ToString());
+                    isSuccess = true;
+                }
+                else
+                {
+                    //failed to execute query
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //Close the connection 
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+    }
+}
---
+++ AnyStore/DAL/transactionDetailDAL.cs
@@ -0,0 +1,73 @@
+﻿using AnyStore.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore.DAL
+{
+    class transactionDetailDAL
+    {
+        //Create Connection String
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        #region Insert Method for Transaction Detail
+        public bool InsertTransactionDetail(transactionDetailBLL td)
+        {
+            //Create a boolean value and set its default value to false
+            bool isSuccess = false;
+
+            //Create a database connection here
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //Sql Query to Insert Transaction detais
+                string sql = "INSERT INTO tbl_transaction_detail (product_id, rate, qty, total, dea_cust_id, added_date, added_by) VALUES (@product_id, @rate, @qty, @total, @dea_cust_id, @added_date, @added_by)";
+
+                //Passing the value to the SQL Query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //Passing the values using cmd
+                cmd.Parameters.AddWithValue("@product_id", td.product_id);
+                cmd.Parameters.AddWithValue("@rate", td.rate);
+                cmd.Parameters.AddWithValue("@qty", td.qty);
+                cmd.Parameters.AddWithValue("@total", td.total);
+                cmd.Parameters.AddWithValue("@dea_cust_id", td.dea_cust_id);
+                cmd.Parameters.AddWithValue("@added_date", td.added_date);
+                cmd.Parameters.AddWithValue("@added_by", td.added_by);
+
+                //Open Database connection
+                conn.Open();
+
+                //declare the int variable and execute the query
+                int rows = cmd.ExecuteNonQuery();
+
+                if(rows>0)
+                {
+                    //Query Executed Successfully
+                    isSuccess = true;
+                }
+                else
+                {
+                    //FAiled to Execute Query
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //Close Database Connection
+                conn.Close();
+            }
+            return isSuccess;
+        }
+        #endregion
+    }
+}
